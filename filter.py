95dd8f5f1b74b8f1191330ad1ef504f3326911e1
Maksim125 <yamaksim@sas.upenn.edu>
2019-03-02 01:09:23 -0800

    added ability to compare lists for similarities

--- filter.py
+++ filter.py
@@ -1,8 +1,11 @@
 import wikipedia
 import spacy
+from operator import itemgetter
+
+top_number = 2
+nlp = spacy.load('en_core_web_md')
 
 def clean(page_text):
-	nlp = spacy.load('en_core_web_md')
 	doc = nlp(wikipedia.page(page_text).content)
 	filtered_text = []
 	for token in doc:
@@ -12,6 +15,49 @@ def clean(page_text):
 		    #else:
 		    filtered_text.append(token)
 	return filtered_text
+
+def to_tokens_list(string_list):
+	concated_list = " ".join(string_list)
+	print(concated_list)
+	
+	print(concated_list)
+
+	doc = nlp(concated_list)
+	return doc
+
+def top_list(tuple_list):
+	top_scores = []
+	for i in range(top_number):
+		max_tuple = max(tuple_list, key = lambda x: x[1])
+		print(max_tuple)
+		top_scores.append(max_tuple)
+		tuple_list.remove(max_tuple)
+	return top_scores
+
+def most_relevant(list_links, list_words):
+	list_links = to_tokens_list(list_links)
+	list_words = to_tokens_list(list_words)
+	linked_scores = []
+	for link in list_links:
+		sim = 0
+		for word in list_words:
+			sim += link.similarity(word)
+		linked_scores.append((link,sim))
+	return top_list(linked_scores)
+
+
+
+
+x = ["boo","bleg","bloop","blorp"]
+y = ["dasd","sdasd","asdas","asdasd","hgrwfw"]
+print(most_relevant(x,y))
+#list of links
+#compares against list of words from a target page
+#Find the top 20
+
+
+
+
 
 
 #class filter:
