27b86ee4cc9fb47eaa748e0acf3e543fccec275e
Maksim125 <yamaksim@sas.upenn.edu>
2019-03-02 16:49:58 -0800

    temp mod

--- graphics.py
+++ graphics.py
@@ -2,9 +2,21 @@ from tkinter import *
 import tkinter.messagebox
 
 
+def update_graphics(status_string):
+	status = Label(root, text = status_string, bd=1,relief=SUNKEN, anchor = W)
+	
+
+
+
+
+root = Tk()
+root.title("Traveling-Wikipedia-Problem")
+
+status.pack(side=BOTTOM, fill=X)
 
 
 
 
 
 
+root.mainloop()
